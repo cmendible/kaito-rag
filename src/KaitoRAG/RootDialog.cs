25b53b479e0a072365a14fc774591e696cffb9c4
Rodrigo Liberoff Vázquez <rliberoff@encamina.com>
2024-07-19 19:52:25 +0200

    Better prompt engineer to get the proper information from context.

--- src/KaitoRAG/RootDialog.cs
+++ src/KaitoRAG/RootDialog.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
 
 using KaitoRAG.Extensions;
 using KaitoRAG.Services;
@@ -10,6 +11,10 @@ namespace KaitoRAG;
 
 internal class RootDialog(RootDialogConfiguration configuration) : Dialog
 {
+    // Handles the removal of a footnote reference only when it's not followed by its corresponding footnote.
+    private static readonly Regex ResponseCleanRegexPattern = new(@"\[\^([^\]]+)\](?=\s*$)(?!.*?\[\^\1\]:)", RegexOptions.Compiled | RegexOptions.Singleline, TimeSpan.FromSeconds(3));
+    private static readonly Regex ResponseExtractReferenceNumbersPattern = new(@"\[\$(\d+)\$\]", RegexOptions.Compiled, TimeSpan.FromSeconds(3));
+
     /// <inheritdoc/>>
     public override async Task<DialogTurnResult> BeginDialogAsync(DialogContext dc, object options = null, CancellationToken cancellationToken = default)
     {
@@ -25,75 +30,74 @@ internal class RootDialog(RootDialogConfiguration configuration) : Dialog
         }
 
         var searchRecordsCollection = await Task.WhenAll(configuration.UserSearchService.SearchRecordsAsync(query, userId, cancellationToken), configuration.GlobalSearchService.SearchRecordsAsync(query, cancellationToken));
-        var searchRecords = searchRecordsCollection.Where(x => x != null).SelectMany(x => x!);
+        var searchRecords = searchRecordsCollection.Where(x => x != null).SelectMany(x => x!).ToList(); ;
 
-        var systemPrompt = $$"""
-            Answer the question below.
-            Take into consideration your chat history.
-            Never use your own knowledge, only use the context below.
-            If you don't know the answer, just say that `I don't know`.
-            Keep the answer as concise as possible.
-            ONLY use the CHAT HISTORY as additional context to understand what the user is asking you about, avoid using it to format your answer.
-            Never forget to add the SOURCE URL from the context and only use `https://www.bing.com` if you don't know the answer.
-            
-            The format of your response must always be: 
-                <ANSWER>[^<A NUMBER STARTING FROM 1>]
-
-                [^<A NUMBER STARTING FROM 1>]: <SORUCE URL>
-            
-            ---
-            {{BuildChatHistoryPromptSection(configuration.ChatHistoryService.Retrieve(userId))}}
+        if (searchRecords.Count == 0)
+        {
+            await turnContext.SendActivityAsync(MessageFactory.Text(@"I'm sorry, I don't know about that!"), cancellationToken);
+        }
+        else
+        {
+            var systemPrompt = $$"""
             {{BuildContextPromptSection(searchRecords)}}
+            
+            Use the previous pieces of information to answer the question below with the following instructions:
+              1. Select the most relevant information from the context.
+              2. Generate a draft response concatenating every selected piece of information, ensuring it is precise and concise. 
+                2a. If the piece of information reference one context, use this format: `<Your answer>[$<ID Context>$]`
+                2b. If the piece of information reference multiple contexts, use this format: `<Your answer>[$<ID Context>$],[$<ID other Context>$],[$<ID another Context>$],...`
+              3. Remove duplicate content from the draft response
+              4. Generate your final response after adjusting it to increase accuracy and relevant.
+              5. Now only show your final response! Do not provide any explanations or details
+            
+            
             QUESTION: {{query}}
             ANSWER:
             """;
 
-        var response = await configuration.KaitoService.GetInferenceAsync(systemPrompt, cancellationToken: cancellationToken);
+            var response = await configuration.KaitoService.GetInferenceAsync(systemPrompt, cancellationToken: cancellationToken);
 
-        await turnContext.SendActivityAsync(MessageFactory.Text(response), cancellationToken);
+            var cleanupResponse = CleanUpString(response);
+            var references = ExtractReferenceNumbers(cleanupResponse);
 
-        var now = DateTime.UtcNow;
+            await turnContext.SendActivityAsync(MessageFactory.Text(cleanupResponse), cancellationToken);
 
-        await configuration.ChatHistoryService.InsertAsync(new ChatHistoryRecord()
-        {
-            Content = query,
-            DateTimeUtc = now,
-            Id = Guid.NewGuid(),
-            Role = ChatHistoryRecord.ChatHistoryRecordRole.User,
-            UserId = userId,
-        }, cancellationToken);
+            var now = DateTime.UtcNow;
 
-        await configuration.ChatHistoryService.InsertAsync(new ChatHistoryRecord()
-        {
-            Content = response,
-            DateTimeUtc = now,
-            Id = Guid.NewGuid(),
-            Role = ChatHistoryRecord.ChatHistoryRecordRole.Assistant,
-            UserId = userId,
-        }, cancellationToken);
+            await configuration.ChatHistoryService.InsertAsync(new ChatHistoryRecord()
+            {
+                Content = query,
+                DateTimeUtc = now,
+                Id = Guid.NewGuid(),
+                Role = ChatHistoryRecord.ChatHistoryRecordRole.User,
+                UserId = userId,
+            }, cancellationToken);
+
+            await configuration.ChatHistoryService.InsertAsync(new ChatHistoryRecord()
+            {
+                Content = cleanupResponse,
+                DateTimeUtc = now,
+                Id = Guid.NewGuid(),
+                Role = ChatHistoryRecord.ChatHistoryRecordRole.Assistant,
+                UserId = userId,
+            }, cancellationToken);
+
+        }
 
         return await dc.EndDialogAsync(cancellationToken: cancellationToken);
     }
 
     private static string BuildContextPromptSection(IEnumerable<SearchRecord> searchRecords)
     {
-        if (!searchRecords.Any())
-        {
-            return "CONTEXT: N/A\n\n---\n";
-        }
-
-        var sb = new StringBuilder();
-        var index = 1; // Start index at 1 for human-readable indexing
+        var sb = new StringBuilder("<<BEGIN CONTEXT>>\n\n");
+        var index = 0; // Start index at 1 for human-readable indexing
 
         foreach (var record in searchRecords)
         {
-            sb.AppendLine($"CONTEXT {index}:\n{record.Content}\n")
-              .AppendLine($"SOURCE URL {index}: {record.Url}\n");
-
-            index++;
+            sb.AppendLine($"CONTEXT ID {++index}:\n\n{record.Content}\n");
         }
 
-        return sb.Append("---\n").ToString();
+        return sb.AppendLine("<<END CONTEXT>>").ToString();
     }
 
     private static string? BuildChatHistoryPromptSection(IEnumerable<ChatHistoryRecord> chatHistoryRecords)
@@ -103,10 +107,30 @@ internal class RootDialog(RootDialogConfiguration configuration) : Dialog
             return null;
         }
 
-        var chatHistory = string.Join(Environment.NewLine,
-                                      chatHistoryRecords.Select(record => $@"{(record.Role == ChatHistoryRecord.ChatHistoryRecordRole.User ? @"QUESTION" : @"ANSWER")}: {record.Content}" +
-                                                                              (record.Role == ChatHistoryRecord.ChatHistoryRecordRole.Assistant ? "\n" : string.Empty)));
+        var chatHistory = string.Join("\n", chatHistoryRecords.Select(record => $@"{(record.Role == ChatHistoryRecord.ChatHistoryRecordRole.User ? @" - USER" : @" - ASSISTANT")}: {record.Content}"));
 
-        return $"\nCHAT HISTORY:\n\n{chatHistory}\n---\n";
+        return $"User conversation history:\n\n{chatHistory}\n---\n";
+    }
+
+    private static string CleanUpString(string input)
+    {
+        return (ResponseCleanRegexPattern.IsMatch(input) ? ResponseCleanRegexPattern.Replace(input, string.Empty) : input).Trim();
+    }
+
+    private static HashSet<int> ExtractReferenceNumbers(string input)
+    {
+        var referenceNumbers = new HashSet<int>();
+
+        var matches = ResponseExtractReferenceNumbersPattern.Matches(input);
+
+        foreach (Match match in matches)
+        {
+            if (match.Success)
+            {
+                referenceNumbers.Add(int.Parse(match.Groups[1].Value));
+            }
+        }
+
+        return referenceNumbers;
     }
 }
