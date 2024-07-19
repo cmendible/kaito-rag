d120a81a6a8ce46c155bc7b9f96a03a0dd3f4445
Rodrigo Liberoff Vázquez <rliberoff@encamina.com>
2024-07-19 09:11:38 +0200

    Updated outputs from Azure App Config.

---
+++ infra/terraform/modules/appcs/outputs.tf
@@ -0,0 +1,9 @@
+output "id" {
+  description = "Specifies the resource ID of the Azure App Configuration."
+  value       = azurerm_app_configuration.appcs.id
+}
+
+output "primary_read_key_connection_string" {
+  description = "Specifies the resource ID of the Azure App Configuration."
+  value       = azurerm_app_configuration.appcs.primary_read_key.0.connection_string
+}
--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -12,3 +12,8 @@ output "aks_node_resource_group_name" {
   description = "The name of the node Resource Group on the Azure Kubernetes Service (AKS) cluster"
   value       = data.azurerm_resource_group.node_resource_group.name
 }
+
+output "appcs_primary_read_connection_string" {
+  description = "The primary read connection string to the Azure App Configuration"
+  value       = module.appcs.primary_read_key_connection_string
+}
