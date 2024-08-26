21d254784f8ae1b040509efdd41ba2bbf4b2aa46
Rodrigo Liberoff Vázquez <rliberoff@encamina.com>
2024-08-26 14:45:25 +0200

    The same Terraform module can be used now to deploy manage or upstream versions of Kaito.

--- infra/terraform/modules/kaito_upstream/main.tf
+++ infra/terraform/modules/kaito_upstream/main.tf
@@ -1,31 +1,58 @@
 data "azurerm_subscription" "current" {
 }
 
-data "azurerm_user_assigned_identity" "kaito_identity" {
-  name                = var.kaito_identity_name
-  resource_group_name = var.kaito_identity_resource_group_name
-}
-
 resource "azurerm_role_assignment" "kaito_provisioner_assigned_identity_contributor_role" {
   principal_id         = data.azurerm_user_assigned_identity.kaito_identity.principal_id
   scope                = var.aks_id
   role_definition_name = "Contributor"
 }
 
-resource "kubernetes_namespace_v1" "kaito_namespace" {
+resource "kubernetes_namespace" "kaito_namespace" {
   metadata {
     name = var.kaito_aks_namespace
   }
 }
 
+resource "azapi_update_resource" "enable_kaito" {
+  count       = var.use_upstream_version ? 0 : 1
+  type        = "Microsoft.ContainerService/managedClusters@2024-03-02-preview"
+  resource_id = var.aks_id
+
+  body = jsonencode({
+    properties = {
+      aiToolchainOperatorProfile = {
+        enabled = true
+      }
+    }
+  })
+}
+
+data "azurerm_user_assigned_identity" "kaito_identity" {
+  name                = var.kaito_identity_name
+  resource_group_name = var.kaito_identity_resource_group_name
+
+  depends_on = [azapi_update_resource.enable_kaito]
+}
+
+resource "azurerm_federated_identity_credential" "kaito_federated_identity_credential" {
+  name                = "id-federated-kaito"
+  resource_group_name = data.azurerm_user_assigned_identity.kaito_identity.resource_group_name
+  parent_id           = data.azurerm_user_assigned_identity.kaito_identity.id
+  issuer              = var.aks_oidc_issuer_url
+  audience            = ["api://AzureADTokenExchange"]
+  subject             = var.use_upstream_version ? "system:serviceaccount:gpu-provisioner:gpu-provisioner" : "system:serviceaccount:kube-system:kaito-gpu-provisioner"
+}
+
 resource "helm_release" "kaito_workspace" {
+  count            = var.use_upstream_version ? 1 : 0
   name             = "kaito-workspace"
   chart            = "${path.module}/charts/kaito/workspace/"
-  namespace        = kubernetes_namespace_v1.kaito_namespace.metadata.0.name
+  namespace        = kubernetes_namespace.kaito_namespace.metadata.0.name
   create_namespace = false
 }
 
 resource "helm_release" "gpu_provisioner" {
+  count = var.use_upstream_version ? 1 : 0
   name  = "kaito-gpu-provisioner"
   chart = "https://github.com/Azure/gpu-provisioner/raw/gh-pages/charts/gpu-provisioner-${var.gpu_provisioner_version}.tgz"
   wait  = true
@@ -116,37 +143,13 @@ resource "helm_release" "gpu_provisioner" {
   }
 }
 
-resource "azurerm_federated_identity_credential" "workload_federated_identity_credential" {
-  name                = "id-federated-kaito"
-  resource_group_name = var.resource_group_name
-  parent_id           = data.azurerm_user_assigned_identity.kaito_identity.id
-  issuer              = var.aks_oidc_issuer_url
-  subject             = "system:serviceaccount:gpu-provisioner:gpu-provisioner"
-  audience            = ["api://AzureADTokenExchange"]
-}
-
-resource "azurerm_network_security_rule" "kaito_ai_model_inference_network_security_rule" {
-  # # count                       = var.kaito_use_load_balancer ? 1 : 0
-  name                        = "rule-${var.kaito_aks_namespace}-${var.kaito_inference_port}"
-  priority                    = 100
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = 80
-  source_address_prefix       = "Internet"
-  destination_address_prefix  = "*"
-  resource_group_name         = var.resource_group_name
-  network_security_group_name = var.network_security_group_name
-}
-
-resource "kubectl_manifest" "kaito_ai_model_workspace" {
+resource "kubectl_manifest" "kaito_ai_model" {
   yaml_body = <<-EOF
     apiVersion: kaito.sh/v1alpha1
     kind: Workspace
     metadata:
       name: kaito-${var.kaito_ai_model}
-      namespace: ${kubernetes_namespace_v1.kaito_namespace.metadata.0.name}
+      namespace: ${kubernetes_namespace.kaito_namespace.metadata.0.name}
       annotations:
         kaito.sh/enablelb: "False"
     resource:
@@ -160,7 +163,25 @@ resource "kubectl_manifest" "kaito_ai_model_workspace" {
         name: "${var.kaito_ai_model}"
     EOF
 
-  depends_on = [helm_release.gpu_provisioner] # Forcefully set a dependecy with the GPU provider, to ensure that any Kaito AI model will be deployed correctly with the GPU.
+  depends_on = [
+    azapi_update_resource.enable_kaito,
+    helm_release.kaito_workspace,
+    helm_release.gpu_provisioner
+  ]
+}
+
+resource "azurerm_network_security_rule" "kaito_ai_model_inference_network_security_rule" {
+  name                        = "rule-${var.kaito_aks_namespace}-${var.kaito_inference_port}"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = 80
+  source_address_prefix       = "Internet"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = var.network_security_group_name
 }
 
 resource "kubernetes_ingress_v1" "kaito_ai_model_inference_endpoint_ingress" {
@@ -168,7 +189,7 @@ resource "kubernetes_ingress_v1" "kaito_ai_model_inference_endpoint_ingress" {
 
   metadata {
     name      = "ingress-kaito-${var.kaito_ai_model}"
-    namespace = kubernetes_namespace_v1.kaito_namespace.metadata.0.name
+    namespace = kubernetes_namespace.kaito_namespace.metadata.0.name
     annotations = {
       "kubernetes.io/ingress.class" = "addon-http-application-routing"
     }
