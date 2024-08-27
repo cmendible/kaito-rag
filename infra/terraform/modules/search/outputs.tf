06cdd1d8dc9fec9aea31fd3bf2e71a6fee977724
Rodrigo Liberoff Vázquez <rliberoff@encamina.com>
2024-08-27 20:10:31 +0200

    Fixed the Azure AI Search key returned as output of the Terraform script.

--- infra/terraform/modules/search/outputs.tf
+++ infra/terraform/modules/search/outputs.tf
@@ -1,5 +1,5 @@
 output "key" {
   description = "The Primary Key used for the Azure AI Search service."
-  value       = azurerm_search_service.search.query_keys[0].key
+  value       = azurerm_search_service.search.primary_key
   sensitive   = true
 }
